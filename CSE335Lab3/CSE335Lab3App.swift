772c8889e20b4b21f3b75719f5a42d81b9ad4a4d
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-02-14 18:33:19 -0700

    Initial Commit

---
+++ CSE335Lab3/CSE335Lab3App.swift
@@ -0,0 +1,17 @@
+//
+//  CSE335Lab3App.swift
+//  CSE335Lab3
+//
+//  Created by Arjun Dadhwal on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct CSE335Lab3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
