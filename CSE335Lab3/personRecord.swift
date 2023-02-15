ba62366861c8215d803e0b01a1d70fdaa2d5f003
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-02-14 18:41:31 -0700

    created record

---
+++ CSE335Lab3/personRecord.swift
@@ -0,0 +1,26 @@
+//
+//  personRecord.swift
+//  CSE335Lab3
+//
+//  Created by Arjun Dadhwal on 2/14/23.
+//
+
+import Foundation
+class personRecord
+{
+    var name:String? = nil
+    var ssn:Int64? = nil
+    var age:Int16? = nil
+    
+    init(n:String, s:Int64, a:Int16) {
+        self.name = n
+        self.ssn = s
+        self.age = a
+    }
+    
+    func change_age(newAge:Int16)
+    {
+        self.age = newAge;
+    }
+    
+}
