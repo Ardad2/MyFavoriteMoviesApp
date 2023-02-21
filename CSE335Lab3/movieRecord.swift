504cf547e92180ca24d51aaf2a37e621e7edfead
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-02-21 14:05:37 -0700

    redoing the app

---
+++ CSE335Lab3/movieRecord.swift
@@ -0,0 +1,16 @@
+//
+//  movieRecord.swift
+//  CSE335Lab3
+//
+//  Created by Arjun Dadhwal on 2/21/23.
+//
+
+import Foundation
+
+class movieRecord
+{
+    var title:String? = nil;
+    var genre:String? = nil;
+    var price:Int16? = nil;
+    
+}
