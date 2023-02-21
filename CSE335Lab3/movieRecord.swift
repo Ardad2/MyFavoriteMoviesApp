af454fc1b2087a95849abe9d7fcc4ba9acf19b4f
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-02-21 14:08:34 -0700

    created the movie record object

--- CSE335Lab3/movieRecord.swift
+++ CSE335Lab3/movieRecord.swift
@@ -13,4 +13,21 @@ class movieRecord
     var genre:String? = nil;
     var price:Int16? = nil;
     
+    init(new_title:String, new_genre: String, new_price:Int16) {
+        self.title = new_title;
+        self.genre = new_genre;
+        self.price = new_price;
+    }
+    
+    func change_genre(new_genre: String)
+    {
+        self.genre = new_genre;
+    }
+    
+    func change_price(newPrice:Int16)
+    {
+        self.price = newPrice;
+    }
+    
+    
 }
