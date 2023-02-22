dd1be6d4f0a63400bc9d79edadbbe1f93d700b2d
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-02-22 10:57:45 -0700

    set up navigation view

--- CSE335Lab3/ContentView.swift
+++ CSE335Lab3/ContentView.swift
@@ -15,4 +15,61 @@ struct ContentView: View {
         
     }
     
+    struct NaviView: View
+    {
+        @Binding var titleN:String
+        @Binding var genreN:String
+        @Binding var priceN:String
+        
+        @State var showingDeleteAlert = false
+        @Binding var deleteTitle: String
+        @ObservedObject var movieModel : movieArray
+
+        var body: some View
+        {
+                Text("")
+                   .toolbar{
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action:
+                        {
+                            print(movieModel.getCount())
+                                                        
+                            movieModel.add_movie(titleN, (genreN), Int16(priceN) ?? 0)
+                        },
+                        label: {
+                            Image(systemName: "plus.app")
+                        })
+                    }
+                    
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button(action:
+                        {
+                            print(titleN)
+                            showingDeleteAlert = true
+                        },
+                               label: {
+                            Image(systemName: "trash")
+                        })
+                    }
+                   }.alert("Delete Record", isPresented: $showingDeleteAlert, actions: {
+                       TextField("Enter Title", text: $deleteTitle)
+
+                       Button("Delete", action: {
+                           
+                           movieModel.delete_movie(title: deleteTitle);
+                           
+                           showingDeleteAlert = false
+                           
+                       })
+                       Button("Cancel", role: .cancel, action: {
+                           showingDeleteAlert = false
+                       })
+                   }, message: {
+                       Text("Please enter Title to Search.")
+                   })
+            
+            }
+
+    }
+    
 }
